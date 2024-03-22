ccb85dbe70873a3df3e309d3541687ec76e68f78
Divakar Yadav <divakaryadav@Divakars-MBP.lan>
2024-03-22 17:58:25 -0500

    21th commit

--- src/main/java/org/example/MediQueryConfiguration.java
+++ src/main/java/org/example/MediQueryConfiguration.java
@@ -1,31 +1,8 @@
 package org.example;
 
-        import com.fasterxml.jackson.annotation.JsonProperty;
-        import io.dropwizard.Configuration;
-        import io.dropwizard.jetty.HttpConnectorFactory;
-        import io.dropwizard.server.DefaultServerFactory;
 
-        import javax.validation.Valid;
-        import javax.validation.constraints.NotNull;
-        import java.util.Collections;
+import io.dropwizard.Configuration;
 
 public class MediQueryConfiguration extends Configuration {
-
-    @Valid
-    @NotNull
-    private HttpConnectorFactory httpConnector = new HttpConnectorFactory();
-
-    @JsonProperty("server")
-    public DefaultServerFactory getServerFactory() {
-        DefaultServerFactory serverFactory = new DefaultServerFactory();
-        serverFactory.setApplicationConnectors(Collections.singletonList(httpConnector));
-        return serverFactory;
-    }
-
-    @JsonProperty("server")
-    public void setServerFactory(DefaultServerFactory serverFactory) {
-        // This method is necessary for Dropwizard to correctly parse the configuration
-    }
-
-    // You can add other configuration properties here
+    // You can add any necessary configuration properties here
 }
