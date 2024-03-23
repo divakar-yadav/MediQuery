fe87f16c543371c096f695b640281e02010b18d4
Divakar Yadav <divakaryadav@Divakars-MBP.lan>
2024-03-23 01:37:30 -0500

    27th commit

--- src/main/java/org/example/MediQuery.java
+++ src/main/java/org/example/MediQuery.java
@@ -3,6 +3,10 @@ package org.example;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 import org.example.resources.MediQueryResource;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class MediQuery extends Application<MediQueryConfiguration> {
 
@@ -14,6 +18,16 @@ public class MediQuery extends Application<MediQueryConfiguration> {
     public void run(MediQueryConfiguration configuration, Environment environment) {
         // Register your resource classes here
         environment.jersey().register(new MediQueryResource());
-        environment.jersey().register(CorsFilter.class);
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,PUT,DELETE,OPTIONS");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping for the filter
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 }
